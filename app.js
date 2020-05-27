3f314e716d89fa48515a44c516331114391ddfae
Pablo Fiorido <pablofiorido@hotmail.com>
2020-05-27 09:54:29 -0300

    parcial 1: css style added, code corrections

--- app.js
+++ app.js
@@ -7,59 +7,96 @@ var emailError = null
 var age = null
 var ageError = null
 var gender = null
-var genderError = null
 var topic = null
 var topicError = null
 var country = null
-var countryError = null
+var comments = null
+var commentsError = null
 var sendBtn = null
-
-var handleInputChange = function (evt) {
-    console.log(name1.value)
-    console.log(lastname.value)
-}
+var radios = null
 
 var sendForm = function (evt) {
     evt.preventDefault()
     if (name1.value.length < 3) {
         nameError.innerHTML = 'El nombre debe tener al menos 3 caracteres'
+        console.log(name1.value)
     } else {
         nameError.innerHTML = ''
+        console.log(name1.value)
     }
 
     if (lastname.value.length < 3) {
         lastnameError.innerHTML = 'El apellido debe tener al menos 3 caracteres'
+        console.log(lastname.value)
     } else {
         lastnameError.innerHTML = ''
+        console.log(lastname.value)
     }
 
-    if (email.value.length < 3) {
-        emailError.innerHTML = 'El email no esta escrito correctamente'
+    if (validateEmail(email.value) == false) {
+        emailError.innerHTML = 'Email inválido'
+        console.log(email.value)
     } else {
         emailError.innerHTML = ''
+        console.log(email.value)
     }
 
-    if (age.value < 1 && age.value > 99) {
+    if (age.value > 100 || age.value < 1) {
         ageError.innerHTML = 'Edad incorrecta'
+        console.log(age.value)
     } else {
         ageError.innerHTML = ''
+        console.log(age.value)
+    }
+
+    if (gender.value != null) {
+        console.log(gender.value)
+    }
+
+    if (topic.value == null) {
+        topicError.innerHTML = 'Seleccione tema de interes'
+        console.log(topic.value)
+    } else {
+        topicError.innerHTML = ''
+        console.log(topic.value)
+    }
+
+    if (country.value != null) {
+        console.log(country.value)
+    }
+
+    if (comments.value.length < 3) {
+        commentsError.innerHTML = 'Ingrese comentario'
+        console.log(comments.value)
+    } else {
+        commentsError.innerHTML = ''
+        console.log(comments.value)
     }
 
     return false
 }
 
+function validateEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
 window.onload = function () {
     name1 = document.getElementById('name')
-    name1.onchange = handleInputChange
 
     lastname = document.getElementById('lastname')
-    lastname.onchange = handleInputChange
 
     email = document.getElementById('email')
-    email.onchange = handleInputChange
 
     age = document.getElementById('age')
-    age.onchange = handleInputChange
+
+    gender = document.getElementById('gender')
+
+    topic = document.getElementById('topic')
+
+    country = document.getElementById('country')
+
+    comments = document.getElementById('comments')
 
     sendBtn = document.getElementById('send')
     sendBtn.onclick = sendForm
@@ -72,4 +109,13 @@ window.onload = function () {
 
     ageError = document.getElementById('ageError')
 
+    topicError = document.getElementById('topicError')
+
+    commentsError = document.getElementById('commentsError')
+
+}
+
+function validateEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
 }
